2c3060f85445d8dc8d46c5ed3ecf3aca48069e72
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2022-12-24 23:15:50 -0500

    Add files via upload

---
+++ include/color.h
@@ -0,0 +1,16 @@
+typedef struct values
+{
+    int r;
+    int g;
+    int b;
+    int a;
+    
+    int c;
+    int m;
+    int y;
+    int k;
+}
+
+void line(int color, int x, int y);
+
+void rectangle(int color, int x, int y);
