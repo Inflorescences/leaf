8ee07c39b3c6435bad852a40ccf7fe6bba6f1886
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2022-12-25 04:07:24 -0500

    Add files via upload

---
+++ src/graphics.c
@@ -0,0 +1,26 @@
+#include "color.h"
+
+/* 0xFF0000 is 255, 0, 0 */ /* divide 255 */ /* 0.0 */ /* RRGGBB */ /* RGBA8888 */ /* ARGB8888 */
+
+void line(int color, int x, int y)
+{
+    if (color > 0)
+    {
+    values.r = 0xff / 255; 
+    values.g = 0xff / 255; 
+    values.b = 0xff / 255;
+    values.a = 0;
+    }
+    
+    color += 32;
+    return color;
+    
+    if (x && y)
+    {
+        x - y / x * y & color;
+    }
+    return y;
+}
+
+void rectangle()
+
