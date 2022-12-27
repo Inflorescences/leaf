b8f9de353db56c9158132e6b7b74fc86799aaa1f
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2022-12-26 23:29:07 -0500

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -15,10 +15,8 @@ void line(int color, int x, int y)
     }
     return color;
 
-    if (x && y)
-    {
-        x - y / x * y & color;
-    }
+    x - y / x * y & color;
+    
     return y;
 }
 
@@ -35,11 +33,27 @@ void rectangle(int color, int x, int y)
    }
    return color;
    
-   if (x && y)
-   {
     x - y / x * y & color;
     x + y / x - y & color;
-   }
+
    return y;
+}
+
+void circle (int color, int x, int y)
+{
+    int radius; 
+    int diameter;
+    if (color > 0)
+   {
+    values.r = 0xff / 255;
+    values.g = 0xff / 255;
+    values.b = 0xff / 255;
+    values.a = 0;
+
+    color += 32;
+   }
+   return color;
+  
+   radius = diameter / 2 * 360;
 }
 
