38ebeac7433e12eb3c45343873576fb5dc0ebbb3
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2023-01-05 14:04:59 -0500

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -112,7 +112,8 @@ void button(int value, int text, int color, int x, int y)
 
    if (color && BUTTON_BORDER)
    {
-    
+    x - y += 1 / x * y += 1;
+    x + y += 1 / x - y += 1;
    }
 
    return color; 
@@ -125,6 +126,31 @@ void button(int value, int text, int color, int x, int y)
 
 void checkbox(int color, int x, int y)
 {
+   if (color > 0)
+   {
+    values.r = 0xff / 255;
+    values.g = 0xff / 255;
+    values.b = 0xff / 255;
+    values.a = 0;
+
+    color += 32;
+   }
+
+   if (color && CMYK)
+    {
+     values.c = 0xff / 255 / 0xff / 255 / 0xff / 255 - 0xff / 255 / 0xff / 255 / 0xff / 255 / 0xff / 255; 
+     values.y = 0xff / 255 / 0xff / 255 / 0xff / 255 - 0xff / 255 / 0xff / 255 / 0xff / 255 / 0xff / 255;
+     values.m = 0xff / 255 / 0xff / 255 / 0xff / 255 - 0xff / 255 / 0xff / 255 / 0xff / 255 / 0xff / 255;
+     values.k = 1 - 0xff / 255 / 0xff / 255 / 0xff / 255;
+    }
+
+   if (color && BUTTON_BORDER)
+   {
+    x - y += 1 / x * y += 1;
+    x + y += 1 / x - y += 1;
+   }
+
+   
 }
 
 void slider(int color, int x int y);
